bee001ce92deacacf7a8e5ea2243269baca782a4
michel.alves <michelsantosa@gmail.com>
2025-01-03 11:29:46 -0300

    add itens on template

--- placarfutmesa/base/views.py
+++ placarfutmesa/base/views.py
@@ -2,7 +2,46 @@
 from django.shortcuts import render
 
 
+class Competition:
+    def __init__(self, comp_id, comp_type, comp_name, comp_stage, comp_modality, comp_status):
+        self.slug = "".join(comp_name.split())
+        self.comp_id = comp_id
+        self.comp_type = comp_type
+        self.comp_name = comp_name
+        self.comp_stage = comp_stage
+        self.comp_modality = comp_modality
+        self.comp_status = comp_status
+
+
+competitions = [
+        Competition(
+            20250319,
+            'Campeonato',
+            'Campeonato interno',
+            'Etapa 2',
+            'Cavado',
+            'Progress'
+        ),
+        Competition(
+            20250410,
+            'Copa',
+            'Copa COP',
+            '',
+            'Cavado',
+            'Progress'
+        ),
+        Competition(
+            20250205,
+            'Copa',
+            'Circulão',
+            '',
+            'Cavado',
+            'Closed'
+        ),
+]
+
+
 # Create your views here.
 def home(request):
     # return HttpResponse('<html><body>PLACAR - FUTEBOL DE MESA</html></body>', content_type='text/html')
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', context={'competitions': competitions})
